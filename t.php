1eb3ac0db9484e6ca6341c24a58a618304cd45b8
Varshini-i10 <71812935+Varshini-i10@users.noreply.github.com>
2020-09-27 14:39:45 +0530

    Uploaded

---
+++ t.php
@@ -0,0 +1,8 @@
+<?php
+$states="Miss Alabam texas";
+$statesArray=[];
+$states1=explode(' ',$states);
+echo "orginal array <br>";
+foreach($states as $i=>$value)
+	print("states[$i]=$value<br>");
+?>
